72467f64b0b6e45c6fa9aeef7a3947cea929670f
Gridoptic <kirill.vihrev.98@gmail.com>
2026-06-04 21:55:52 +0700

    Add files via upload

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'https://stakeholders-quad-lawyer-similar.trycloudflare.com';
+const API_BASE_URL = 'https://purchases-acquisition-modular-founded.trycloudflare.com';
 
 const tg = window.Telegram?.WebApp;
 
